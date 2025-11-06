4447c0159653521638118abaabf0760a37749af4
abila <abila@rizenlaptop>
2025-11-06 09:18:43 +0530

    changes commited

---
+++ SpringIocDi/src/main/java/com/tnsif/SpringIocDi/SpringIocDiApplication.java
@@ -0,0 +1,13 @@
+package com.tnsif.SpringIocDi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIocDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIocDiApplication.class, args);
+	}
+
+}
---
+++ SpringIocDi/src/test/java/com/tnsif/SpringIocDi/SpringIocDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnsif.SpringIocDi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIocDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
