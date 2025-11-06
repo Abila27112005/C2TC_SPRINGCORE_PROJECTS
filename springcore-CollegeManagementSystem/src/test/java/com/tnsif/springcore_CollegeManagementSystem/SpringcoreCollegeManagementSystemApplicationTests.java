c129145e5c4e9ac40a551c7d8144af9a98f908f3
abila <abila@rizenlaptop>
2025-11-06 16:02:27 +0530

    changes commit

---
+++ springcore-CollegeManagementSystem/src/test/java/com/tnsif/springcore_CollegeManagementSystem/SpringcoreCollegeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnsif.springcore_CollegeManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoreCollegeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
